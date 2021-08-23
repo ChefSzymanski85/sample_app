2f8521128a0adb7fd1b63f993a76a64e7f363011
Mark Szymanski <chefszymanski85@gmail.com>
2021-08-23 18:22:12 +0000

    Added pages and refactored code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
+# classes are simply a convenient way to organize functions...
 class StaticPagesController < ApplicationController
+  #  like the home and help actions, which are defined using the def keyword
   def home
   end
 
   def help
   end
+  
+  def about
+  end
+  
+  def contact
+  end
+
 end
