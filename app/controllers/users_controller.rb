08acfd855dc403702f38e457f45200059623e526
Mark Szymanski <chefszymanski85@gmail.com>
2022-03-14 00:03:40 +0000

    implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,10 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params) # Not the final implementation!
-    if @user.save == false
+    @user = User.new(user_params)
+    if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -22,7 +24,7 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+                                   :password_confirmation)
     end
   
 end
